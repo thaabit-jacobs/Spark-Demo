352f78ac2f4eac7124a7acbddd7b69dca82a3d36
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-07-28 11:27:55 +0200

    added constructor to UserExecption

--- src/main/java/spark/user/exceptions/UserException.java
+++ src/main/java/spark/user/exceptions/UserException.java
@@ -2,5 +2,13 @@ package spark.user.exceptions;
 
 public class UserException extends Exception
 {
-    
+    public UserException()
+    {
+        super();
+    }
+
+    public UserException(String errMsg)
+    {
+        super(errMsg);
+    }
 }
